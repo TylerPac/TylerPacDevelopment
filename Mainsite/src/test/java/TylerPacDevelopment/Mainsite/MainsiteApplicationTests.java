72bde572570b0a2999eefa6f865c68d0ca64a6bf
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-07-23 02:12:37 -0400

    push

--- Mainsite/src/test/java/TylerPacDevelopment/Mainsite/MainsiteApplicationTests.java
+++ Mainsite/src/test/java/TylerPacDevelopment/Mainsite/MainsiteApplicationTests.java
@@ -1,4 +1,4 @@
-package tylerpac.dev.mainsite;
+package TylerPacDevelopment.Mainsite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+
+How to run my website locally
+
+mvn clean 
+java -jar target\Mainsite-0.0.1-SNAPSHOT.jar
