0326932092c0773e3efc5cfead87d5bca9a02399
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-07-23 00:59:15 -0400

    initial test with tomcat and springboot

---
+++ mainsite/src/main/java/tylerpac/dev/mainsite/MainsiteApplication.java
@@ -0,0 +1,13 @@
+package tylerpac.dev.mainsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainsiteApplication.class, args);
+	}
+
+}
