7736d8d9f035789edede608ff4bf514946f1f6c9
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-07-23 12:56:28 -0400

    s

--- Mainsite/src/main/java/TylerPacDevelopment/Mainsite/controller/HomeController.java
+++ Mainsite/src/main/java/TylerPacDevelopment/Mainsite/controller/HomeController.java
@@ -1,4 +1,4 @@
-package TylerPacDevelopment.Mainsite.controller;
+package tylerpacdevelopment.mainsite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
