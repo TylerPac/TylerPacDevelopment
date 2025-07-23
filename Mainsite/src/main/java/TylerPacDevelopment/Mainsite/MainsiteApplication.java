6d2824d229b58df01ac594272ec4dc9c38641740
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-07-23 02:12:37 -0400

    push

--- Mainsite/src/main/java/TylerPacDevelopment/Mainsite/MainsiteApplication.java
+++ Mainsite/src/main/java/TylerPacDevelopment/Mainsite/MainsiteApplication.java
@@ -1,4 +1,4 @@
-package tylerpac.dev.mainsite;
+package TylerPacDevelopment.Mainsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
