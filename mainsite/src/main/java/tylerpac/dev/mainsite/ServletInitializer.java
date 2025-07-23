85f2ca3ced984b165d429873349bd130ea8e866f
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-07-23 00:59:15 -0400

    initial test with tomcat and springboot

---
+++ mainsite/src/main/java/tylerpac/dev/mainsite/ServletInitializer.java
@@ -0,0 +1,13 @@
+package tylerpac.dev.mainsite;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainsiteApplication.class);
+	}
+
+}
---
+++ mainsite/src/test/java/tylerpac/dev/mainsite/MainsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package tylerpac.dev.mainsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
