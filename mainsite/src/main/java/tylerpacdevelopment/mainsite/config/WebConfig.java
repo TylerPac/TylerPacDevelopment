1019509c31f2c2a677d3e0abfa358c6cce58e391
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-12-31 12:30:36 -0500

    Tighten CORS origins

--- mainsite/src/main/java/tylerpacdevelopment/mainsite/config/WebConfig.java
+++ mainsite/src/main/java/tylerpacdevelopment/mainsite/config/WebConfig.java
@@ -16,7 +16,6 @@ public class WebConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins(
                                 "http://localhost:3000",
-                                "http://localhost:3000",
                                 "http://localhost:5173",
                                 "http://127.0.0.1:3000",
                                 "http://127.0.0.1:5173"
